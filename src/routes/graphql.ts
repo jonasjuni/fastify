6144573640b50758627d81b52b390dce74ff515a
Jonas Cerqueira da Silva Junior <jonas.developer@live.com>
2022-07-16 12:07:03 -0300

    Add prettier and graphql hello world

---
+++ src/routes/graphql.ts
@@ -0,0 +1,35 @@
+import { FastifyPluginAsync } from 'fastify';
+import mercuriusCodegen, { gql } from 'mercurius-codegen';
+import mercurius, { IResolvers } from 'mercurius';
+
+const graphql: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  const schema = gql`
+    type Query {
+      hello(name: String!): String!
+    }
+  `;
+
+  const resolvers: IResolvers = {
+    Query: {
+      hello(root: any, { name }: any, ctx: any, info: any) {
+        // root ~ {}
+        // name ~ string
+        // ctx.authorization ~ string | undefined
+        // info ~ GraphQLResolveInfo
+        return 'hello ' + name;
+      },
+    },
+  };
+
+  fastify.register(mercurius, {
+    schema,
+    resolvers,
+  });
+
+  mercuriusCodegen(fastify, {
+    // Commonly relative to your root package.json
+    targetPath: './src/graphql/generated.ts',
+  }).catch(console.error);
+};
+
+export default graphql;
